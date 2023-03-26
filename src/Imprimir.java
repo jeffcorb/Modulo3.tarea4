167ad06fe3031d514aa083f851f770dea0310d96
jeffcorb <jefferson.corbs@gmail.com>
2023-03-26 18:35:01 -0500

    Modificando Clases

--- src/Imprimir.java
+++ src/Imprimir.java
@@ -1,22 +1,22 @@
 public class Imprimir {
-    public static void imprimirNumeros(int n) {
+    void imprimirNumeros(int n) {
         for (var i = 1; i <= n; i++) {
-            System.out.print(i + " ");
+            System.out.printf(i + " ");
         }
     }
 
-    public static void imprimirPares(int n) {
+    void imprimirPares(int n) {
         for (int i = 1; i <= n; i++) {
             if (i % 2 == 0) {
-                System.out.print(i + " ");
+                System.out.printf(i + " ");
             }
         }
     }
 
-    public static void imprimirImpares(int n) {
+    void imprimirImpares(int n) {
         for (int i = 1; i <= n; i++) {
             if (i % 2 != 0) {
-                System.out.print(i + " ");
+                System.out.printf(i + " ");
             }
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         final var aleatorio = new Aleatorio();
         final var numero = aleatorio.generarRandomInteger(20, 10);
+        imprimirEjercicio(numero);
+    }
+
+    private static void imprimirEjercicio(int numero) {
+
         final var imprimir = new Imprimir();
         System.out.println("lista de numeros");
         imprimir.imprimirNumeros(numero);
