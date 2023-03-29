bb84db9561c43a6a8b339fc21ce867a4d10a1e0c
jeffcorb <jefferson.corbs@gmail.com>
2023-03-28 21:39:20 -0500

    Cambios Mar28

--- src/Imprimir.java
+++ src/Imprimir.java
@@ -1,14 +1,14 @@
 public class Imprimir {
     void imprimirNumeros(int n) {
         for (var i = 1; i <= n; i++) {
-            System.out.printf(i + " ");
+            System.out.printf("%d ", i);
         }
     }
 
     void imprimirPares(int n) {
         for (int i = 1; i <= n; i++) {
             if (i % 2 == 0) {
-                System.out.printf(i + " ");
+                System.out.printf("%d ", i);
             }
         }
     }
@@ -16,7 +16,7 @@ public class Imprimir {
     void imprimirImpares(int n) {
         for (int i = 1; i <= n; i++) {
             if (i % 2 != 0) {
-                System.out.printf(i + " ");
+                System.out.printf("%d ", i);
             }
         }
     }
